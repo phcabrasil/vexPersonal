1bfba90a1e2306c6ca8ffeb47777e999d733b2db
Pedro Amui <curiamuiph@gmail.com>
2018-06-11 16:16:02 -0700

    Help Brendan's Firend

---
+++ pusherMachine/include/main.h
@@ -0,0 +1,30 @@
+
+#ifndef MAIN_H_
+
+// This prevents multiple inclusion, which isn't bad for this file but is good practice
+#define MAIN_H_
+
+#include <API.h>
+
+// Allow usage of this file in C++ programs
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define limit 1
+#define pusher 9
+
+void autonomous();
+
+void initializeIO();
+
+void initialize();
+
+void operatorControl();
+
+// End C++ export structure
+#ifdef __cplusplus
+}
+#endif
+
+#endif
